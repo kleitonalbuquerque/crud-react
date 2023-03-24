52285676b28b0cfd47b7cb5e9c003bcb074a6981
kleitonalbuquerque <kleiton2102@gmail.com>
2023-03-24 19:57:24 -0300

    Componente Cadastro Usuário #02

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,12 @@
 import { Component } from "react";
+import axios from "axios"
 import Main from "../template/Main";
+
+const baseUrl = 'http://localhost:3001/users'
+const initalState = {
+  user: { name: '', email: '' },
+  list: []
+}
 
 const headerProps = {
   icon: "users",
@@ -8,6 +15,30 @@ const headerProps = {
 };
 
 export default class UserCrud extends Component {
+
+  state = { ...initalState }
+
+  clear() {
+    this.setState({ user: initalState.user })
+  }
+
+  save() {
+    const user = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+    axios[method](url, user)
+      .then(res => {
+        const list = this.getUpdatedList(res.data)
+        this.setState({ user: initalState.user, list })
+      })
+  }
+
+  getUpdatedList(user) {
+    const list = this.state.list.filter(u => u.id !== user.id)
+    list.unshift(user)
+    return list
+  }
+
   render() {
     return (
       <Main {...headerProps}>
@@ -16,7 +47,3 @@ export default class UserCrud extends Component {
     )
   }
 }
-
-// export default function UserCrud() {
-//   return <Main {...headerProps}>Cadastro de Usuário</Main>;
-// }
